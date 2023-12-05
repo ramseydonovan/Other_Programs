6d5a9b9f349ad4ca41cdb5b84ab7e17456fa1881
Donovan Ramsey <djramsey1515@gmail.com>
2023-12-05 11:44:36 -0500

    Add files via upload

---
+++ format-results.py
@@ -0,0 +1,511 @@
+results = [
+    {
+       "id":"1699881854459",
+       "text":"Gender?",
+       "answers":[
+          "Female",
+          "Male",
+          "Male",
+          "Female",
+          "Female",
+          "Female",
+          "Male",
+          "Female",
+          "Female",
+          "Male",
+          "Female",
+          "Female",
+          "Male",
+          "Female",
+          "Female",
+          "Male",
+          "Male",
+          "Male",
+          "Male",
+          "Male",
+          "Male",
+          "Male",
+          "Female"
+       ]
+    },
+    {
+       "id":"1699882053344",
+       "text":"Select your age:",
+       "answers":[
+          "18 - 29",
+          "30 - 39",
+          "18 - 29",
+          "18 - 29",
+          "50 - 64",
+          "50 - 64",
+          "30 - 39",
+          "30 - 39",
+          "30 - 39",
+          "30 - 39",
+          "40 - 49",
+          "30 - 39",
+          "18 - 29",
+          "50 - 64",
+          "50 - 64",
+          "18 - 29",
+          "18 - 29",
+          "18 - 29",
+          "30 - 39",
+          "30 - 39",
+          "18 - 29",
+          "30 - 39",
+          "18 - 29"
+       ]
+    },
+    {
+       "id":"1699882158284",
+       "text":"Do you use a fitness app?",
+       "answers":[
+          "yes",
+          "no",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "yes",
+          "no",
+          "yes",
+          "yes",
+          "yes",
+          "no",
+          "no",
+          "yes",
+          "yes",
+          "yes",
+          "yes"
+       ]
+    },
+    {
+       "id":"1699882686880",
+       "text":"What fitness app(s) do you use?",
+       "answers":[
+          "Apple Fitness",
+          "MyFitnessPal",
+          "Apple Fitness",
+          "MyFitnessPal",
+          "MyFitnessPal",
+          "MyFitnessPal",
+          "other",
+          "Apple Fitness;other",
+          "MyFitnessPal;FitOn",
+          "Nike Training Club",
+          "Apple Fitness;Nike Training Club;Strava",
+          "other",
+          "other",
+          "None",
+          "other",
+          "Apple Fitness",
+          "Apple Fitness;other",
+          "None",
+          "None",
+          "Strava;other",
+          "Strava",
+          "other",
+          "None"
+       ]
+    },
+    {
+       "id":"1699882801924",
+       "text":"If Other, please specify",
+       "answers":[
+          "",
+          "",
+          "",
+          "",
+          "Yoga Apps",
+          "",
+          "Peloton",
+          "Tempo and peloton (apple everyday, others occasionally) ",
+          "",
+          "",
+          "",
+          "Samsung health care",
+          "Samsung health",
+          "",
+          "IFit",
+          "",
+          "Keep",
+          "",
+          "",
+          "FitBit",
+          "",
+          "LG Health, came with my phone and i mainly use it to track steps",
+          "Healthify me"
+       ]
+    },
+    {
+       "id":"1699882841795",
+       "text":"How long have you been using this app?",
+       "answers":[
+          "1-2 years",
+          "Less than 1 year",
+          "Less than 1 year",
+          "more than 4 years",
+          "2-4 years",
+          "more than 4 years",
+          "more than 4 years",
+          "more than 4 years",
+          "1-2 years",
+          "Less than 1 year",
+          "more than 4 years",
+          "2-4 years",
+          "1-2 years",
+          "Less than 1 year",
+          "more than 4 years",
+          "Less than 1 year",
+          "1-2 years",
+          "Less than 1 year",
+          "Less than 1 year",
+          "2-4 years",
+          "Less than 1 year",
+          "more than 4 years",
+          "Less than 1 year"
+       ]
+    },
+    {
+       "id":"1699882998000",
+       "text":"How frequently you use the app?",
+       "answers":[
+          "Less than once a month",
+          "Less than once a month",
+          "A few times a month",
+          "A few times a month",
+          "A few times a month",
+          "a few times a week",
+          "once a week",
+          "everyday",
+          "a few times a week",
+          "once a week",
+          "A few times a month",
+          "everyday",
+          "everyday",
+          "Never",
+          "a few times a week",
+          "A few times a month",
+          "a few times a week",
+          "Never",
+          "Never",
+          "more than once per day",
+          "a few times a week",
+          "Less than once a month",
+          "a few times a week"
+       ]
+    },
+    {
+       "id":"1699966554749",
+       "text":"Do you workout with your phone on your person?",
+       "answers":[
+          "Sometimes",
+          "Sometimes",
+          "Never",
+          "Never",
+          "Never",
+          "Always",
+          "Sometimes",
+          "Never",
+          "Sometimes",
+          "Sometimes",
+          "Sometimes",
+          "Always",
+          "Always",
+          "Never",
+          "Never",
+          "Always",
+          "Always",
+          "Never",
+          "Never",
+          "Sometimes",
+          "Always",
+          "Always",
+          "Sometimes"
+       ]
+    },
+    {
+       "id":"1699966616193",
+       "text":"Where do you exercise most?",
+       "answers":[
+          "Gym",
+          "Home",
+          "Gym",
+          "Gym",
+          "Home",
+          "Gym",
+          "Outdoors",
+          "Home",
+          "Home",
+          "Gym",
+          "Gym",
+          "Outdoors",
+          "Gym",
+          "Home",
+          "Home",
+          "Outdoors",
+          "Outdoors",
+          "Gym",
+          "Gym",
+          "Outdoors",
+          "Outdoors",
+          "Home",
+          "Gym"
+       ]
+    },
+    {
+       "id":"1699966677642",
+       "text":"How often do you consider that you are using proper technique when exercising?",
+       "answers":[
+          "Frequently",
+          "Never",
+          "Frequently",
+          "Every exercise",
+          "Never",
+          "Frequently",
+          "Frequently",
+          "Frequently",
+          "Every exercise",
+          "Every exercise",
+          "Frequently",
+          "Sometimes",
+          "Every exercise",
+          "Frequently",
+          "Every exercise",
+          "Sometimes",
+          "Frequently",
+          "Every exercise",
+          "Never",
+          "Sometimes",
+          "Every exercise",
+          "Frequently",
+          "Sometimes"
+       ]
+    },
+    {
+       "id":"1699966755704",
+       "text":"Have you ever experienced an exercise related injury?",
+       "answers":[
+          "No",
+          "No",
+          "No",
+          "Yes",
+          "No",
+          "No",
+          "Yes",
+          "Yes",
+          "Yes",
+          "Yes",
+          "Yes",
+          "Yes",
+          "Yes",
+          "No",
+          "No",
+          "Yes",
+          "No",
+          "Yes",
+          "Yes",
+          "Yes",
+          "Yes",
+          "Yes",
+          "Yes"
+       ]
+    },
+    {
+       "id":"1699966794899",
+       "text":"How interested would you be in a feature that provides real-time feedback on your posture during workouts using video analysis?",
+       "answers":[
+          "Interested",
+          "Not Interested",
+          "Interested",
+          "Interested",
+          "Very interested",
+          "Somewhat interested",
+          "Neutral",
+          "Interested",
+          "Very interested",
+          "Very interested",
+          "Interested",
+          "Interested",
+          "Interested",
+          "Very interested",
+          "Neutral",
+          "Neutral",
+          "Somewhat interested",
+          "Not Interested",
+          "Not Interested",
+          "Interested",
+          "Very interested",
+          "Neutral",
+          "Interested"
+       ]
+    },
+    {
+       "id":"1699966910330",
+       "text":"If not interested any particular reason why? (Optional)",
+       "answers":[
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "Over integration of technology ",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "I don't think it would be accurate",
+          "",
+          "",
+          "",
+          "",
+          "",
+          ""
+       ]
+    },
+    {
+       "id":"1699966939043",
+       "text":"If interested any particular reason? (Optional)",
+       "answers":[
+          "",
+          "",
+          "",
+          "Just to know that your form is correct to prevent injuries and get the most out of your workout ",
+          "So I can improve my health ",
+          "Always good to see if form stays consistent throughout reps",
+          "Most of my activity does not relate to this type of tracking. Running, cycling, swimming. Collect data via wearable tech",
+          "I have that on the Tempo app and it’s nice to be corrected so that I and working out efficiently and effectively. ",
+          "just to make sure I am doing it properly to get the maximum benefits",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          ""
+       ]
+    }
+ ]
+
+do_you_use_a_fitness_app = '1699882158284'
+how_long_have_you_been_using_app = '1699882841795'
+injury_related_id_key = '1699966755704'
+
+def update_results(question_id, answer):
+    for i,question in enumerate(results):
+        if results[i]['id'] == question_id:
+            results[i]['new_answers'].append(answer)
+
+def isExpertUser(user_dict, do_you_use_a_fitness_app, how_long_have_you_been_using_app):
+    return user_dict[do_you_use_a_fitness_app] == 'yes' and user_dict[how_long_have_you_been_using_app] != 'Less than 1 year'
+
+
+def update_results_for_injured_user():
+    for user in users.keys():
+        if users[user].get(injury_related_id_key, '') != 'Yes':
+            continue
+        # add all answers for non filtered users
+        for question_id in users[user].keys():
+            update_results(question_id, users[user].get(question_id))
+
+def update_results_for_expert_users_only():
+    for user_id in users.keys():
+        if not isExpertUser(users[user_id], do_you_use_a_fitness_app, how_long_have_you_been_using_app):
+            continue
+        # add all answers for non filtered users
+        for question_id in users[user_id].keys():
+            update_results(question_id, users[user_id].get(question_id))
+
+def update_results_for_novice_users_only():
+    for user_id in users.keys():
+        if isExpertUser(users[user_id], do_you_use_a_fitness_app, how_long_have_you_been_using_app):
+            continue
+        # add all answers for non filtered users
+        for question_id in users[user_id].keys():
+            update_results(question_id, users[user_id].get(question_id))
+
+# add new key for filtered answers
+for i,question in enumerate(results):
+    if not results[i].get('new_answers'):
+        results[i]['new_answers'] = []
+
+# create dict of {user_id: { question_id: answer}}
+users = {}
+for question in results:
+    for i, ans in enumerate(question['answers']):
+        if not users.get(i):
+            users[i] = {}
+        users[i][question['id']] = ans
+
+
+# apply filter and update results
+# NOTE: to apply filter set below boolean to True and then uncomment a filter function
+use_filter = True
+if use_filter:
+    # update_results_for_expert_users_only()
+    # update_results_for_injured_user()
+    update_results_for_novice_users_only()
+
+for question in results:
+    print('------------------------------------------------')
+    print(question['text'])
+    # list of each users answer for single question
+    answers = []
+    filtered_answers = []
+    answers = question['answers']
+    filtered_answers = question.get('new_answers', [])
+
+    
+    # { answer: count}
+    counts = {}
+    for answer in answers:
+        if not answer:
+            continue
+        if counts.get(answer):
+            counts[answer] += 1
+        else:
+            counts[answer] = 1
+    
+    filtered_counts = {}
+    for answer in filtered_answers:
+        if not answer:
+            continue
+        if filtered_counts.get(answer):
+            filtered_counts[answer] += 1
+        else:
+            filtered_counts[answer] = 1
+
+    # Sort based on count, so sorting on most popular answer
+    # print results
+    counts = {k: v for k, v in sorted(counts.items(), key=lambda item: item[1], reverse=True)}
+    for ans in counts.keys():
+        count = counts[ans]
+        percent = int(count/len(answers) * 100)
+        output_string_for_answer = f'{ans}: {count} ({percent}%)'
+
+        if use_filter:
+            filtered_count = filtered_counts.get(ans, 0) # return 0 if no answers
+            filtered_percent = int(filtered_count/len(filtered_answers) * 100)
+            output_string_for_answer += f' ({filtered_percent}%)'
+
+        print(output_string_for_answer)
